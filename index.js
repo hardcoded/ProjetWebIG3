69b2c685fa0130d34d82a77e64e6090fe7ddcf76
JohanBrunet <brunet.johan.23@gmail.com>
2016-05-04 16:43:21 +0200

    Create navbar for index

--- index.js
+++ index.js
@@ -5,31 +5,21 @@ var logger = require('morgan');
 var cookieparser = require('cookie-parser');
 var cookiesession = require('cookie-session');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT));
 
 app.use(express.static(__dirname + '/public'));
-app.use(logger('logs'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  response
   res.render('pages/index');
-});
-
-app.get('/times', function(req, res) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  res.send(result);
 });
 
 app.get('/db', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM compte', function(err, result) {
       done();
       if (err) {
         console.error(err);
