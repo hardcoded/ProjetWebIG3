148154c86a8a1344d1c03671922c6db4e4742998
JohanBrunet <brunet.johan.23@gmail.com>
2016-05-09 14:54:12 +0200

    Modify index
    Test database connection + add POST route

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var logger = require('morgan');
-var cookieparser = require('cookie-parser');
-var cookiesession = require('cookie-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -19,19 +22,28 @@ app.get('/', function(req, res) {
 
 app.get('/db', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM compte', function(err, result) {
+    client.query('SELECT * FROM rank', function(err, result) {
       done();
       if (err) {
         console.error(err);
         res.send("Error" + err);
       }
       else {
-        res.render('pages/db', {results: results.rows} );
+        res.render('pages/db', {results: result.rows} );
       }
     });
   });
-})
+});
+
+app.post('/signup', function(req, res) {
+  console.log(req.body.user.firstName);
+  console.log(req.body.user.lastName);
+  console.log(req.body.user.mail);
+  console.log(req.body.user.phone);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+module.exports = app;
