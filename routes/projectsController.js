baed07a0ce0f276180da053b9277027468db161d
JohanBrunet <brunet.johan.23@gmail.com>
2016-05-12 18:09:09 +0200

    Add status to response

--- routes/projectsController.js
+++ routes/projectsController.js
@@ -3,9 +3,11 @@ module.exports.controller = function(app, DAO) {
   app.get('/projects', function(req, res) {
     DAO.getAll({
       success : function(result) {
+        res.status(200);
         res.render('pages/projects', {title: 'Projects', projects: result.rows});
       },
       fail : function(err) {
+        res.status(404);
         res.render('pages/error');
       }
     });
@@ -15,10 +17,12 @@ module.exports.controller = function(app, DAO) {
   app.get('/projects/:id', function(req, res) {
     DAO.getById(req.params.id, {
       success : function(result) {
+        res.status(200);
         res.render('pages/projectDetails', {title: 'Project details', project: result});
       },
       fail : function(err) {
-        res.render('pages/error');
+        res.status(404);
+        res.render('pages/error', {error: err});
       }
     });
   });
@@ -27,6 +31,7 @@ module.exports.controller = function(app, DAO) {
   app.post('/projects/add', function(req, res) {
     DAO.create(project, {
       success : function(result) {
+        res.status(200);
         res.render('pages/projects', {title: 'Project details', projects: result.rows});
       },
       fail : function(err) {
