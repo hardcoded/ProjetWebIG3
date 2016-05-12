2a84d4e133c9004bff32b096d93e3bb76f31fbf4
JohanBrunet <brunet.johan.23@gmail.com>
2016-05-12 02:33:19 +0200

    Update controller to handle project POJO

--- routes/projectsController.js
+++ routes/projectsController.js
@@ -12,10 +12,22 @@ module.exports.controller = function(app, DAO) {
   });
 
   /* GET projects page */
-  app.get('/projects/:id', function(req, res) {
+  app.get('/projects/:id/infos', function(req, res) {
     DAO.getById(req.params.id, {
       success : function(result) {
-        res.render('pages/projectDetails', {title: 'Project details', projects: result.rows});
+        res.render('pages/projectDetails', {title: 'Project details', project: result});
+      },
+      fail : function(err) {
+        res.render('pages/error');
+      }
+    });
+  });
+
+  /* POST project */
+  app.post('/projects/add', function(req, res) {
+    DAO.create(project, {
+      success : function(result) {
+        res.render('pages/projects', {title: 'Project details', projects: result.rows});
       },
       fail : function(err) {
         res.render('pages/error');
